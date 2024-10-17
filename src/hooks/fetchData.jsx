e29121eae330fe9f372f8587229eb9c959f55960
unknown <hendratanjaya1@gmail.com>
2024-10-17 23:31:41 +0700

    also minor changes

--- src/hooks/fetchData.jsx
+++ src/hooks/fetchData.jsx
@@ -10,8 +10,8 @@ export async function fetchHistory(session_id){
             if(apikey === "-1" && await fetchedData.api_key){
                 localStorage.setItem("session_id",fetchedData.api_key);
             }
-            //console.log(await fetchedData);
-            return fetchedData;
+            //console.log(fetchedData);
+            return fetchedData.history[0];
         }
 
     }catch(error){
@@ -32,15 +32,17 @@ export async function addNewGame(newHistory){
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(newHistory[newHistory.length-1])
         });
-        if(response.ok){
-            console.log(response);
+        if(response){
+            //console.log(response);
             return
         }
     } catch (error) {
         console.error("Error:", error.message);
         throw error;
     }
+    //console.log(JSON.stringify(newHistory[newHistory.length-1]))
 }
+
 
 export async function updateHistory(activeGame, idx){
 
@@ -52,7 +54,7 @@ export async function updateHistory(activeGame, idx){
             body: JSON.stringify({game:activeGame, idx: idx})
         });
         if(response.ok){
-            console.log(response);
+            //console.log(response);
             return
         }
     } catch (error) {
